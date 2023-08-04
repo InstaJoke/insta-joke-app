13eaff89885189517329301e8c6dc7aa3e04ed0a
Lucas Sulivan <lucas.sulivan@hotmail.com>
2023-08-04 01:32:52 +0100

    My first commit relacioned the sign in page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import NavBar from "./assets/components/nav-bar/NavBar"
 import ExplorePage from "./assets/pages/explorePage/ExplorePage"
+import SignInPage from "./assets/pages/sign-in-page/Sign-in-page"
 import { Routes,Route } from "react-router-dom"
 import "./App.css"
 
@@ -9,6 +10,7 @@ function App() {
     <article>
       <Routes>
         <Route path="/explore" element={<ExplorePage />}/>
+        <Route path="/" element={<SignInPage/>} />
       </Routes>
       <NavBar />
     </article>
