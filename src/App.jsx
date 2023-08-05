3b6904328e7f3d2172faefa3b13edf9e40373ad0
Vanessa Dallaris <dallarisvanessa@yahoo.com>
2023-08-05 20:05:21 +0200

    Add profile page , favicon and adjust the see more button in explore

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import NavBar from "./assets/components/nav-bar/NavBar"
 import ExplorePage from "./assets/pages/explorePage/ExplorePage"
 import SignInPage from "./assets/pages/sign-in-page/Sign-in-page"
 import WeeklyJoke from "./assets/pages/Weekly-joke/WeeklyJoke"
+import ProfilePage from "./assets/pages/Profile-page/ProfilePage"
 
 import { Routes,Route } from "react-router-dom"
 import "./App.css"
@@ -15,6 +16,7 @@ function App() {
         <Route path="/explore" element={<ExplorePage />}/>
         <Route path="/" element={<SignInPage/>} />
         <Route path="/weekly" element={<WeeklyJoke/>}/>
+        <Route path="/profile" element={<ProfilePage/>}/>
 
       </Routes>
       <NavBar />
